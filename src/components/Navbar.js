1fed1a8119325b26156ad5007fbd551c591566ec
sm8sk4l <sebastianmoskal@protonmail.com>
2023-03-15 00:39:14 +0100

    nav mobile done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,44 @@
-import { IoPersonCircle, IoCart } from "react-icons/io5";
+import { useState } from "react";
+import {
+  IoReorderFourOutline,
+  IoCloseOutline,
+  IoCartOutline,
+} from "react-icons/io5";
 
 const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
   return (
-    <div className="text-xl font-[Robotto] border-b">
-      <nav className="flex items-center  px-12 py-8 ">
-        <a href="/" className="mr-auto">
-          LOGO
-        </a>
-        <ul className="flex gap-8 uppercase mr-auto">
-          <li>Men</li>
-          <li>Women</li>
-          <li>Kids</li>
-          <li>Brands</li>
-          <li>Sale</li>
-        </ul>
-        <div className="flex gap-6 text-2xl ">
-          <button>
-            <IoPersonCircle />
-          </button>
-          <button>
-            <IoCart />
+    <nav className="flex items-end justify-center flex-col ">
+      <div className="flex w-screen justify-between items-center px-6 py-4">
+        <p className="font-bold text-5xl font-jost">ALDO</p>
+
+        <div className="sm:hidden">
+          <button onClick={handleClick} className="text-4xl">
+            {clicked ? <IoCloseOutline /> : <IoReorderFourOutline />}
           </button>
         </div>
-      </nav>
-    </div>
+      </div>
+
+      <div className="w-screen ">
+        <ul
+          className={`text-sm 
+           mobile
+          ${clicked ? "flex" : "hidden"} 
+          sm:flex sm:flex-row sm:justify-end`}
+        >
+          <li>Overview</li>
+          <li>Profile</li>
+          <li>My Preferences</li>
+          <li>Address Book</li>
+          <li>Saved Cards</li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
