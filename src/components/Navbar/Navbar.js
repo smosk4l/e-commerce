dacf48b734cfb784a447f2ed6c8319a58a41aab5
sm8sk4l <sebastianmoskal@protonmail.com>
2023-04-02 13:17:52 +0200

    fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
         <div className="w-screen  relative">
           <ul
             className={`text-sm  bg-white
-           mobile fixed h-full  z-50
+           mobile fixed top-20 h-full  z-50
           ${
             clicked ? "translate-x-0" : "translate-x-full"
           } easy-out duration-300
